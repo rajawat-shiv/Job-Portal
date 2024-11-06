4f2d3e9a7109d1eeb3feef4447451c9310557c41
rajawat-shiv <shiv1999rajawat@gmail.com>
2024-11-06 15:33:33 +0530

    "Switched API endpoints from localhost to production URLs"

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,10 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
-// export const USER_API_END_POINT="https://job-portal-backend-omg3.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT="https://job-portal-backend-omg3.onrender.com/api/v1/job";
-// export const APPLICATION_API_END_POINT="https://job-portal-backend-omg3.onrender.com/api/v1/application";
-// export const COMPANY_API_END_POINT="https://job-portal-backend-omg3.onrender.com/api/v1/company";
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+
+
+export const USER_API_END_POINT="https://job-portal-l794.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-l794.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-l794.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-l794.onrender.com/api/v1/company";
